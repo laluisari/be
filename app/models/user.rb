18a756e468a57019bdd8f210f712804702951f93
Ahmad Ramadhan Auni <aramadhan395@gmail.com>
2023-04-03 13:47:49 +0700

    add validation for User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
     member: 1,
     data_expert: 2
   }
+
+  #validations
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 3}, if: -> { new_record? || !password.nil? }
+  validates :role, presence: true
 end
