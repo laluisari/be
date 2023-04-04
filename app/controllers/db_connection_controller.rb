795d1d8d9e254da39622c84b5ed96a443508d0e3
afanokta <afanoktafianto99@gmail.com>
2023-04-04 12:45:15 +0700

    add functions on db_connection_controller

--- app/controllers/db_connection_controller.rb
+++ app/controllers/db_connection_controller.rb
@@ -1,8 +1,47 @@
 class DbConnectionController < ApplicationController
+  before_action :set_db_connection, only: %i[show update destroy]
+
+  def index
+    @dbs = DbConnection.all
+    render json: @dbs.map(&:new_attr), status: :ok
+  end
+
+  def show
+    render json: @db.new_attr, status: :ok
+  end
+
+  def create
+    @db = DbConnection.new(db_connection_params)
+    return render json: { message: 'Failed' }, status: :unprocessable_entity unless @db.save
+
+    render json: @db.map(&:new_attr), status: :ok
+  end
+
+  def update
+    unless @db.update(db_connection_params)
+      render json: { message: 'Update Failed' }, status: :unprocessable_entity
+      return
+    end
+    render json: @db, status: :ok
+  end
+
+  def destroy
+    unless @db.destroy
+      render json: { message: 'Delete Failed' }, status: :unprocessable_entity
+      return
+    end
+    render json: @db, status: :ok
+  end
+
   private
 
   def db_connection_params
     params.require(:db_connection).permit(:user_id, :display_name, :connection_type, :db_port, :db_name, :db_user,
       :db_pass, :analytics_account_id, :json_file, :csv_file)
+  end
+
+  def set_db_connection
+    @db = DbConnection.find_by_id(params[:id])
+    return render json: { message: 'record not found' }, status: :not_found if @db.nil?
   end
 end
