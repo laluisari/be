6d553e9eaccd42ed39cc149c29202c4854729a74
afanokta <afanoktafianto99@gmail.com>
2023-04-04 08:08:16 +0700

    adding user and user_dashboard model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_132723) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_03_061304) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dashboards", force: :cascade do |t|
+    t.string "display_name"
+    t.string "dashboard_type"
+    t.integer "page"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "db_connections", force: :cascade do |t|
     t.integer "user_id"
@@ -25,6 +33,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_132723) do
     t.integer "analytics_account_id"
     t.string "json_file"
     t.string "csv_file"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_dashboards", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "dashboard_id"
+    t.integer "permission"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
