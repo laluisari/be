0604f82f5dcdf9b9ac8c5cace8d99fce572a3a4a
Ahmad Ramadhan Auni <aramadhan395@gmail.com>
2023-04-05 10:48:32 +0700

    Add encryption to password on users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_050011) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_05_032437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,10 +84,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_050011) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "cards", "dashboards", on_delete: :cascade
