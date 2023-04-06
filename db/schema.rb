58005f3350ef39995d2fb71129e046e862793bf4
Ahmad Ramadhan Auni <aramadhan395@gmail.com>
2023-04-06 14:01:38 +0700

    add columns to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_05_041151) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_063215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -89,6 +89,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_05_041151) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.bigint "phone_number"
+    t.string "occupation"
   end
 
   add_foreign_key "cards", "dashboards", on_delete: :cascade
