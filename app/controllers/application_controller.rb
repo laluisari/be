d83ea1e00bff5d3fb75bbb25029a1bae4f836f11
afanokta <afanoktafianto99@gmail.com>
2023-04-06 12:27:08 +0700

    add authenticate_request for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  include JsonWebToken
+  
+  before_action :authenticate_request
+  # before_action :db_connect
+
+  attr_reader :current_user
+
+  private
+
+  def authenticate_request
+    @current_user = JsonWebToken::AuthorizeApiRequest(request.headers)
+    render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+  end
 end
