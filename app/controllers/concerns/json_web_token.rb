cfcd8772c1ba66c59914b3ce5e3a3ca5f75b9241
afanokta <afanoktafianto99@gmail.com>
2023-04-06 12:27:53 +0700

    create json_web_token.rb for handling JWT

---
+++ app/controllers/concerns/json_web_token.rb
@@ -0,0 +1,30 @@
+require 'jwt'
+require 'dotenv/load'
+
+module JsonWebToken
+  SECRET_KEY = ENV['SECRET_KEY']
+
+  def self.AuthorizeApiRequest(headers = {})
+    if headers['Authorization'].present?
+      token = headers['Authorization'].split(' ').last
+      decoded_auth_token = JsonWebToken.decode(token)
+      if decoded_auth_token
+        user = User.find(decoded_auth_token[:user_id])
+        return user if user
+      end
+    end
+    nil
+  end
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.decode(token)
+    body = JWT.decode(token, SECRET_KEY)[0]
+    HashWithIndifferentAccess.new body
+  rescue
+    nil
+  end
+end
