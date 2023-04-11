cd24b89d8dceea4deac0a0b9cf8281a97c86d976
afanokta <afanoktafianto99@gmail.com>
2023-04-12 02:18:16 +0700

    add config for mailers to fix mailers deploy

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,36 @@
+require 'dotenv/load'
 class UserMailer < ApplicationMailer
-  default :from => "me@mydomain.com"
+
+  after_action :set_delivery_options
+
+  default from: "me@mydomain.com"
+
   def registration_confirmation(user)
-      @user = user
-      mail(:to => "#{user.name} <#{user.email}>", :subject => "Registration Confirmation")
+    @user = user
+    @base_url = ENV['BASE_URL']
+    mail to: "#{user.name} <#{user.email}>",
+         subject: "Registration Confirmation"
   end
 
   def forgot_password(user)
-    @user = user 
-    mail to: user.email, subject: 'Reset your password'
+    @user = user
+    mail to: user.email, subject: "Reset your password"
   end
-  
+
+  private
+
+  def set_delivery_options
+    mail.delivery_method :smtp,
+                         {
+                           address: "smtp.gmail.com",
+                           port: 587,
+                           domain: "dafbin-staging.fly.dev",
+                           user_name: "dafbin.qatros@gmail.com",
+                           password: "vxmqlztxgvtoawxl",
+                           authentication: "plain",
+                           enable_starttls_auto: true,
+                           open_timeout: 5,
+                           read_timeout: 5
+                         }
+  end
 end
