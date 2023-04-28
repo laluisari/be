385719ab7c48c16b072a8e568413608889663369
Ahmad Ramadhan Auni <aramadhan395@gmail.com>
2023-04-28 13:21:03 +0700

    remove birthdate on user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_28_034924) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_061948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -98,7 +98,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_28_034924) do
     t.datetime "reset_sent_at"
     t.boolean "accept_policy", default: false
     t.string "avatar"
-    t.date "birthdate"
     t.integer "gender"
   end
 
