221ad7ef5eb9afbf874c63c61dc4fb23dfb5d928
afanokta <afanoktafianto99@gmail.com>
2023-05-01 19:41:02 +0700

    comment skip_before_action

--- app/controllers/generates_controller.rb
+++ app/controllers/generates_controller.rb
@@ -1,8 +1,8 @@
 require 'csv'
 class GeneratesController < ApplicationController
-  skip_before_action :verify_authenticity_token
-  
-  def index 
+  # skip_before_action :verify_authenticity_token
+
+  def index
     render "generates/index"
   end
 
@@ -13,21 +13,21 @@ class GeneratesController < ApplicationController
     else
       csv_data = CSV.parse(file_contents, headers: true)
       array_of_hashes = csv_data.map(&:to_hash)
-      
+
       array_of_hashes.each do |connection_info|
         # Connect to the database using the connection information
         conn = DbConnect.InitConnection(connection_info)
-        
+
         if conn.nil?
           render json: {message: "error: failed to connect to database with #{connection_info.inspect}"}
           return
         end
-        
+
         # Perform any necessary database operations here using the 'conn' object
-        
+
         conn.close
       end
-      
+
       # Redirect the user to a success page
       render json: "berhasil", status: :ok
     end
